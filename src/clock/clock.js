0ce451c1d5455db50aaa79a9d0428b688bf56500
Angel Wang <angel@angelcube.dev>
2025-09-14 09:46:24 -0600

    fix: clock formatting

--- src/clock/clock.js
+++ src/clock/clock.js
@@ -22,12 +22,12 @@ function displayTime() {
   const m = d.getMinutes()
   const s = d.getSeconds()
 
-  const day = d.toLocaleDateString(process.env.CLOCK_LOCALE, {weekday: 'short'}).toUpperCase()
+  const day = d.toLocaleDateString(process.env.LOCALE, {weekday: 'short'}).toUpperCase()
   const hour = h < 10 ? `0${h}` : h
   const minute = m < 10 ? `0${m}` : m
   const second = s < 10 ? `0${s}` : s
 
-  document.querySelector('.pp-clock-time').innerHTML = `${day} : ${hour} : ${minute} : ${second}`
+  document.querySelector('.pp-clock-time').innerHTML = `${day} ${hour} : ${minute} : ${second}`
 
   setTimeout(displayTime, 1000)
 }
@@ -40,7 +40,7 @@ function displayDate() {
   const today = new Date()
   const options = {year: 'numeric', month: 'short', day: 'numeric'}
 
-  document.querySelector('.pp-clock-date').innerHTML = today.toLocaleDateString(process.env.CLOCK_LOCALE, options)
+  document.querySelector('.pp-clock-date').innerHTML = today.toLocaleDateString(process.env.LOCALE, options)
 }
 
 /**
